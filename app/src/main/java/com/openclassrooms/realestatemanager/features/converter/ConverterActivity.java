5bef9002dd2e7437cc88b8a974f72262f7b1ae08
Marc <marc-fleury@outlook.fr>
2020-08-04 12:32:58 +0200

    Adding layout of ConverterActivity

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/features/converter/ConverterActivity.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.realestatemanager.features.converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.openclassrooms.realestatemanager.R;
+
+public class ConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
